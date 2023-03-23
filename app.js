7836b69dd2686284eff11734cc62769a4ef87ba4
Nurick <k.nurik.y@mail.ru>
2023-03-23 15:03:30 +0300

    Упражнение - проверка робота

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('HelloHello World!');
+const ans = prompt('Сколько будет 7 + или - 15?');
+
+switch(ans) {
+    case '7':
+    case '8':
+    case 'Я не робот':
+        console.log('Успех');
+        break;
+    default:
+        console.log('Вы робот!')
+}
