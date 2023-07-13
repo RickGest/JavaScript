d8766ecda7ca4a9460acb4dc6e748de8f392c646
nkaziakhmedov <nkaziakhmedov@rvision.ru>
2023-07-13 16:20:40 +0300

    11 and 12 homework

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello Hello World!');
+const card = "6165165165173513646451";
+
+console.log(card);
+
+function hideCard(card) {
+    const res = card.slice(-4)
+                    .padStart(card.length, '*');
+
+    return res;
+}
+
+
+console.log(hideCard(card));
