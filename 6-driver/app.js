5074c4a56ecc915b67a3160051d432168037e54e
nkaziakhmedov <nkaziakhmedov@rvision.ru>
2023-08-10 10:34:19 +0300

    Fix conditon

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -1,6 +1,6 @@
 const hasLicense = true;
 const age = 20;
 const isDrunk = false;
-const canDrive = hasLicense && (age > 18) && !isDrunk
+const canDrive = hasLicense && age >= 18 && !isDrunk
 
-console.log(`Can drive? ${canDrive ? 'Yes' : 'No'}`);
+console.log(`Can drive? ${canDrive ? 'Yes.' : 'No.'}`);
