0bc2bdefe18a1ace0529f0fbb00187581408b51e
nkaziakhmedov <nkaziakhmedov@rvision.ru>
2023-08-10 12:48:17 +0300

    refactor for cycle

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,17 +1,16 @@
 const arr = [1, 400 , -5, 10, 0];
 
 function dumbSorting (arr) {
-    for (i in arr) {
-        for (j in arr) {
+    
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
             if (arr[i] < arr[j]) {
-                const tmp = arr[i]
-                arr[i] = arr[j];
-                arr [j] = tmp;
+                [arr[i],arr[j]] = [arr[j], arr[i]];
             }
         }
     }
+
     return arr;
 }
-
 
 console.log(dumbSorting(arr));
