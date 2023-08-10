9a579f475b3d5fa4cdf66e09c231118c02dac36e
nkaziakhmedov <nkaziakhmedov@rvision.ru>
2023-08-10 10:28:58 +0300

    Change let to const

--- 4-navigator/navigator.js
+++ 4-navigator/navigator.js
@@ -1,11 +1,11 @@
 const addressLat = 0;
 const addressLong = 0;
-let positionLat = 5;
-let positionLong = 3;
+const positionLat = 5;
+const positionLong = 3;
 
 function countDistance(x1, y1, x2, y2) {
     return Math.sqrt(((x1 - x2)**2)+((y1 - y2)**2));
 }
 
-let distance = countDistance(positionLat, positionLong, addressLat, addressLong);
+const distance = countDistance(positionLat, positionLong, addressLat, addressLong);
 console.log(distance);
