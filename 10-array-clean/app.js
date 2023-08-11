c26e444f9283662f4a2a188de63e224b6cd7e503
nkaziakhmedov <nkaziakhmedov@rvision.ru>
2023-08-11 15:36:05 +0300

    delete other options, rewrite let to const, delete console.log

--- 10-array-clean/app.js
+++ 10-array-clean/app.js
@@ -1,43 +1,5 @@
 const array = [0, 123, 23, 23, 45, 4, 6, 1, 3, 9, 10];
 
-////////////////////////////////////////////////////////////////////////////
-
-// option 1
-
-// function clearArr (num) {
-//     return function (arr) {
-//         for (index in arr) {
-//             if (arr[index] < num) {
-//                 arr.splice(index, 1);
-//             }
-//         }
-//         return arr;
-//     }
-// }
-
-// let more5 = clearArr(5);
-// console.log(more5(array));
-
-////////////////////////////////////////////////////////////////////////////
-
-// option 2
-
-// const clearArr = num => arr => {
-//     for (index in arr) {
-//         if (arr[index] < num) {
-//             arr.splice(index, 1);
-//         }
-//     }
-//     return arr;
-// }
-
-// let more5 = clearArr(5);
-// console.log(more5(array));
-
-////////////////////////////////////////////////////////////////////////////
-
-// option 3
-
 const moreThan = function (el, num) {
     return el > num;
 }
@@ -45,11 +7,10 @@ const moreThan = function (el, num) {
 const lessThan = (el, num) => el < num;
 
 function clearArr(arr, fn, num) {
-    let filtredArr = [];
+    const filtredArr = [];
 
     for (el of arr) {
         if (!fn(el, num)) {
-            console.log(el);
             filtredArr.push(el);
         }
     }
@@ -58,5 +19,3 @@ function clearArr(arr, fn, num) {
 
 console.log(array);
 console.log(clearArr(array, lessThan, 10));
-
-////////////////////////////////////////////////////////////////////////////
