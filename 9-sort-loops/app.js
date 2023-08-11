d4b0644b2a6b955a69cce6eeb5c991d771be9881
nkaziakhmedov <nkaziakhmedov@rvision.ru>
2023-08-11 15:09:45 +0300

    array is copied, so original doen't change

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,16 +1,16 @@
 const arr = [1, 400 , -5, 10, 0];
 
 function dumbSorting (arr) {
-    
-    for (let i = 1; i < arr.length; i++) {
+    const newArr = [...arr];
+    for (let i = 1; i < newArr.length; i++) {
         for (let j = 0; j < i; j++) {
-            if (arr[i] < arr[j]) {
-                [arr[i],arr[j]] = [arr[j], arr[i]];
+            if (newArr[i] < newArr[j]) {
+                [newArr[i],newArr[j]] = [newArr[j], newArr[i]];
             }
         }
     }
 
-    return arr;
+    return newArr;
 }
 
 console.log(dumbSorting(arr));
