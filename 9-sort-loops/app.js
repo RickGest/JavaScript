60dc99faab6628efbea127ac032409ae119a5820
nkaziakhmedov <nkaziakhmedov@rvision.ru>
2023-08-12 17:52:29 +0300

    rewrite iterations

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -2,8 +2,8 @@ const arr = [1, 400 , -5, 10, 0];
 
 function dumbSorting (arr) {
     const newArr = [...arr];
-    for (let i = 1; i < newArr.length; i++) {
-        for (let j = 0; j < i; j++) {
+    for (let i = 0; i < newArr.length - 1; i++) {
+        for (let j = i + 1; j < newArr.length; j++) {
             if (newArr[i] < newArr[j]) {
                 [newArr[i],newArr[j]] = [newArr[j], newArr[i]];
             }
